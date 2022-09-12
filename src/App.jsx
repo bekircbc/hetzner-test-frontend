d6d2c48f5e8bde8861db1203af7dab893c709839
Bekir - CDI <b.burakcebecii@gmail.com>
2022-09-12 15:07:30 +0200

    geting data from backend.. there is a bug..

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+const baseUrl = "http://localhost:9384";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [employees, setEmployees] = useState();
+
+  useEffect(() => {
+    (async () => {
+      setEmployees((await axios.get(baseUrl)).data);
+    })();
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Employees</h1>
+      {employees.forEach((employee) => {
+        <div className="card">
+          <div>{employee.firstname}</div>
+          <div>{employee.lastname}</div>
+          <div>{employee.department}</div>
+          <div>{employee.age}</div>
+        </div>;
+      })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

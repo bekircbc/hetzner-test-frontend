378b493433ef7ddc400d91af17d67825e4324c50
Bekir - CDI <b.burakcebecii@gmail.com>
2022-09-15 17:41:42 +0200

    changes for caddy

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,9 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
-// const _products = [
-//   {
-//     id: 1,
-//     title: "iPhone 9",
-//     description: "An apple mobile which is nothing like apple",
-//     price: 549,
-//     discountPercentage: 12.96,
-//     rating: 4.69,
-//     stock: 94,
-//     brand: "Apple",
-//     category: "smartphones",
-//     thumbnail: "https://dummyjson.com/image/i/products/1/thumbnail.jpg",
-//     images: [
-//       "https://dummyjson.com/image/i/products/1/1.jpg",
-//       "https://dummyjson.com/image/i/products/1/2.jpg",
-//       "https://dummyjson.com/image/i/products/1/3.jpg",
-//       "https://dummyjson.com/image/i/products/1/4.jpg",
-//       "https://dummyjson.com/image/i/products/1/thumbnail.jpg",
-//     ],
-//   },
-//   {
-//     id: 2,
-//     title: "iPhone 9",
-//     description: "An apple mobile which is nothing like apple",
-//     price: 549,
-//     discountPercentage: 12.96,
-//     rating: 4.69,
-//     stock: 94,
-//     brand: "Apple",
-//     category: "smartphones",
-//     thumbnail: "https://dummyjson.com/image/i/products/1/thumbnail.jpg",
-//     images: [
-//       "https://dummyjson.com/image/i/products/1/1.jpg",
-//       "https://dummyjson.com/image/i/products/1/2.jpg",
-//       "https://dummyjson.com/image/i/products/1/3.jpg",
-//       "https://dummyjson.com/image/i/products/1/4.jpg",
-//       "https://dummyjson.com/image/i/products/1/thumbnail.jpg",
-//     ],
-//   },
-// ];
+const baseUrl = "http://localhost:8324";
 
 function App() {
-  const baseUrl = "http://localhost:8324";
   const [employees, setEmployees] = useState([]);
   useEffect(() => {
     (async () => {
@@ -59,10 +19,10 @@ function App() {
         {employees.map((employee, i) => {
           return (
             <div key={i}>
-              {employee.firstname}
-              {employee.lastname}
-              {employee.department}
-              {employee.age}
+              <div>{employee.firstname}</div>
+              <div>{employee.lastname}</div>
+              <div>{employee.department}</div>
+              <div>{employee.age}</div>
             </div>
           );
         })}
